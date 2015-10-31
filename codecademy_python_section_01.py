84ae0041e6e5b1748f2f9f643211cc402a526b9c
113305 <113305@naver.com>
2015-10-31 15:36:22 +0900

    codecademy_python_section_01

---
+++ codecademy_python_section_01.py
@@ -0,0 +1,55 @@
+#1.1
+print("Welcome to Python!")
+
+#1.2
+my_variable = 10
+
+#1.3
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+#1.4
+my_int = 7
+my_int 3
+print my_int
+
+#1.6
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+
+#1.7
+spam = True
+eggs = False
+
+#1.8
+#wntjr
+mysterious_variable = 42
+
+#1.9
+"""multi"""
+
+#1.10
+count_to = 12+13
+
+print count_to
+
+#1.11
+eggs = 10 ** 2
+
+print eggs
+
+#1.12
+spam = 4 % 3
+
+print spam
+
+#1.13
+#wntjr
+
+monty = True
+python = 1.234
+monty_python = python ** 2 
