3901c3db69bd17e22bddcfa7ba34699cb1c6fd62
113305 <113305@naver.com>
2015-10-31 15:37:29 +0900

    codecademy_python_section_03

---
+++ codecademy_python_section_03.py
@@ -0,0 +1,85 @@
+#3.1
+brian = "Hello life!"
+
+#3.2
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+print caesar
+print praline
+print viking
+
+#3.3
+'This isn\'t flying, this is falling with style!'
+
+#3.4
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
+#3.5
+parrot = "Norwegian Blue"
+len(parrot)
+print len(parrot)
+
+#3.6
+parrot = "Norwegian Blue"
+parrot.lower()
+print parrot.lower()
+
+#3.7
+parrot = "norwegian blue"
+parrot.upper()
+print parrot.upper()
+
+#3.8
+pi = 3.14
+str(pi)
+
+print str(pi)
+
+#3.9
+ministry = "The Ministry of Silly Walks"
+len(ministry)
+ministry.upper()
+print len(ministry)
+print ministry.upper()
+
+#3.10
+print("Monty Python")
+
+#3.11
+the_machine_goes = "Ping!"
+print the_machine_goes
+
+#3.12
+print "Spam " + "and " + "eggs"
+
+#3.13
+print "The value of pi is around " + str(3.14)
+
+#3.15
+name = raw_input("What is your name?")
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
+
+#3.16
+my_string ="red"
+print len(my_string)
+print my_string.upper()
